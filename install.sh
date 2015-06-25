079f12025bed1c76f656c876f6dd3ea21ec44381
rvmn <rvmn@users.noreply.github.com>
2015-06-25 13:46:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ read -t 15
 fi
 
 # add aliases to bashrc of host system
-[ -z $( grep '# docker aliases' ~/.bashrc) ] && curl -fsSL https://raw.githubusercontent.com/rvmn/docker-dev-cloud9/master/docker-alias.sh >> ~/.bashrc && source ~/.bashrc
+[ -z $( grep '# docker aliases' ~/.bashrc) ] && curl -fsSL https://raw.githubusercontent.com/rvmn/docker-dev-cloud9/master/docker-alias.sh >> ~/.bashrc && echo 'source ~/.bashrc' | bash -l
 dalias dcrun 'docker run --privileged -d -v $(pwd):/workspace -p 3000:3000 -p 4000:4000 -p 5000:5000 -p 8181:8181 -p 5901:5901 $1 cloud9'
 # build
 docker build -t cloud9 .
