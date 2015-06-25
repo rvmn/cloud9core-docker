7fa14f595ab8fd40dc6c35bc012041ba0b6650b8
rvmn <rvmn@users.noreply.github.com>
2015-06-25 12:36:09 +0200

    Update and rename prepare.sh to build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+
+. ~/build/config.sh
+
+apt-get update -y
+
+apt-get install $minimal_apt_get_args $BUILD_PACKAGES
+
+# cleanup build
+AUTO_ADDED_PACKAGES=`apt-mark showauto`
+apt-get remove --purge -y $BUILD_PACKAGES $AUTO_ADDED_PACKAGES
+
--- prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -x
-
-. ~/build/config.sh
-
-apt-get update -y
-
-apt-get install $minimal_apt_get_args $BUILD_PACKAGES
